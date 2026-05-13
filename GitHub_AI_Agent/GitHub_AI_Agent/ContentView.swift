116f3bbc53a5bdbe7364017610a6ea4640e1e51b
AR <amit@danfelabs.com>
2026-05-13 16:03:38 -0400

    adding sample app

---
+++ GitHub_AI_Agent/GitHub_AI_Agent/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub_AI_Agent
+//
+//  Created by AR on 2026-05-12.
+//  Copyright © 2026. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub_AI_Agent/GitHub_AI_Agent/GitHub_AI_AgentApp.swift
@@ -0,0 +1,19 @@
+//
+//  GitHub_AI_AgentApp.swift
+//  GitHub_AI_Agent
+//
+//  Created by AR on 2026-05-12.
+//  Copyright © 2026. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct GitHub_AI_AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
